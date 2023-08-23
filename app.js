38f2f40026f51264e9b64feb047ed623398a901e
kaushal sengupta <kaushalsubho2005@gmail.com>
2023-08-23 14:21:49 +0530

    database added

--- app.js
+++ app.js
@@ -14,9 +14,15 @@ const contactconst="In entirely be to at settling felicity. Fruit two match men 
 const aboutconst="Be me shall purse my ought times. Joy years doors all would again rooms these. Solicitude announcing as to sufficient my. No my reached suppose proceed pressed perhaps he. Eagerness it delighted pronounce repulsive furniture no. Excuse few the remain highly feebly add people manner say. It high at my mind by roof. No wonder worthy in dinner."
 posts=[]
 let path=''
+//Mongoose//
+const mongoose= require("mongoose")
+mongoose.connect("mongodb://0.0.0.0:27017/blog",{ useNewUrlParser: true } ).then(()=> console.log("succesfully connected"));
+const BlogSchema= new mongoose.Schema({title:String,body:String,url:String})
+const Blog=mongoose.model("Blog",BlogSchema)
 //logic//
 app.get("/",function(req,res){
-    res.render('index',{hsc:homestconst,listpost:posts,path:path})
+    (Blog.find({})).then(function(blogs){res.render('index',{hsc:homestconst,listpost:blogs,path:path})})
+    
 })
 app.get("/about",function(req,res){
     res.render('about',{ac:aboutconst})
@@ -37,18 +43,19 @@ app.post("/",function(req,res){
     const title=req.body.title
     const cnt=req.body.content
     var post={title:title,post:cnt,url:_.kebabCase(title)}
-    posts.push(post)
+   Blog.insertMany([{title:post.title,body:post.post,url:post.url}])
     res.redirect("/")
 })
 app.get('/posts/:topic',function(req,res){
-    path= _.kebabCase(req.params.topic)
-    console.log(path)
-    posts.forEach(function(element){
+    try{
+    path= _.kebabCase(req.params.topic)}
+    catch(TypeError){}
+    
+    (Blog.find()).then(function(blogs){blogs.forEach(function(element){
         if(_.kebabCase(element.title)==path){
-            res.render("post",{elet:element.title,elep:element.post})
-    }else{console.log("Match not found")}
-})
-res.redirect("/")
+        res.render("post",{elet:element.title,elep:element.body})
+}})})
+
 })
 app.listen(3000,function(){
     console.log("server started on port 3000")
